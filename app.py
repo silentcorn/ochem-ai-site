cd3c9de0192635a40dbda3cfcbe0685cb3427f83
silentcorn <kevingorostieta123@gmail.com>
2025-11-15 19:42:06 -0600

    worked on adding templates, static files (style.css), and began working on python logic

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for
 from rdkit import Chem
 from rdkit.Chem import Draw
 from rdkit.Chem.Draw import rdMolDraw2D
@@ -7,3 +7,19 @@ from DECIMER import predict_SMILES
 # To do: ...
 
 #testing again
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+# Results page route (empty for now)
+@app.route("/results", methods=["GET", "POST"])
+def results():
+    return render_template("results.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
